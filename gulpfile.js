93ea29b1c2c0c16cc55245593be1669943cf8396
Johan Jeppson <johanjeppson@hotmail.com>
2019-08-27 13:59:01 +0200

    upgraded gulp

--- gulpfile.js
+++ gulpfile.js
@@ -19,50 +19,47 @@ const edge = {
     version: edge_manifest.version
 };
 
-gulp.task('packageChrome', function() {
+function packageChrome() {
     console.log(chalk.yellow("Did you update the version in the manifest.json file for Chrome/Firefox? Current version is " + chrome.version));
     return gulp.src(chrome.src)
-         .pipe(zip('chrome_firefox-package-' + chrome.version + '.zip'))
-         .pipe(gulp.dest('dist/packages/'));
-});
+        .pipe(zip('chrome_firefox-package-' + chrome.version + '.zip'))
+        .pipe(gulp.dest('dist/packages/'));
+}
 
 // copy to dist directory
-gulp.task('packageEdge', ['renameManifest', 'copyEdgeResources'], function() {
+function packageEdge() {
     console.log(chalk.yellow("Did you update the version in the ms-manifest.json file for Edge? Current version is " + edge.version));
     return gulp.src('dist/edge_sideload/**/*.*')
         .pipe(zip('edge-package-' + edge.version + '.zip'))
         .pipe(gulp.dest('dist/packages'));
-});
+}
 
 // copy and rename ms-manifest.json -> manifest.json (Edge)
-gulp.task('renameManifest', ['copyFiles'], function() {
-     return gulp.src('src/ms-manifest.json')
-     .pipe(rename('dist/edge_sideload/manifest.json'))
-     .pipe(gulp.dest('.'));
-});
+function renameManifest() {
+    return gulp.src('src/ms-manifest.json')
+        .pipe(rename('dist/edge_sideload/manifest.json'))
+        .pipe(gulp.dest('.'));
+}
 
 // copy files needed for Edge API bridge
-gulp.task('copyEdgeResources', function () {
+function copyEdgeResources() {
     return gulp.src('src/scripts/edge-bridge/**')
         .pipe(gulp.dest('dist/edge_sideload/'));
-});
+}
 
 // copy files for edge
-gulp.task('copyFiles', function() {    
+function copyFiles() {
     return gulp.src(edge.src)
-    .pipe(gulp.dest('dist/edge_sideload/'));
-});
+        .pipe(gulp.dest('dist/edge_sideload/'));
+}
 
 // delete the dist folder
-gulp.task('clean', function() {
+function clean() {
     return del(['dist/**', 'chrome-store/**'], { force: true });
-});
+}
 
-gulp.task('createPackages', ['packageChrome', 'packageEdge'], function() {
-    // remove temp dir
-});
 
-// default tasks
-gulp.task('default', ['clean'], function() {
-    return gulp.start(['createPackages']);
-});
+// EXPORTS 
+exports.default = gulp.series(clean, packageChrome, copyFiles, renameManifest, copyEdgeResources, packageEdge);
+exports.createPackages = exports.default;
+exports.clean = clean;
